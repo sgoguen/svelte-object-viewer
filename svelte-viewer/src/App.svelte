b2222f1907ea3f1d63259a6ac10396a8b81fc3fc
Steve Goguen <sgoguen@gmail.com>
2022-04-20 21:41:50 -0400

    Upgraded Svelte

--- svelte-viewer/src/App.svelte
+++ svelte-viewer/src/App.svelte
@@ -1,8 +1,6 @@
 <script lang="ts">
   import Viewer from "./Viewer.svelte";
 
-  export let name: string;
-
   const person = {
     name: "Steveo",
     age: 27,
@@ -13,37 +11,6 @@
     ],
   };
 </script>
-
-<style>
-  main {
-    text-align: center;
-    padding: 1em;
-    max-width: 240px;
-    margin: 0 auto;
-  }
-
-  h1 {
-    color: #ff3e00;
-    text-transform: uppercase;
-    font-size: 4em;
-    font-weight: 100;
-  }
-
-  @media (min-width: 640px) {
-    main {
-      max-width: none;
-    }
-  }
-</style>
-
-<main>
-  <h1>Hello {name}!</h1>
-  <p>
-    Visit the
-    <a href="https://svelte.dev/tutorial">Svelte tutorial</a>
-    to learn how to build Svelte apps.
-  </p>
-</main>
 
 <h2>Simple Types</h2>
 <Viewer value={1} />
